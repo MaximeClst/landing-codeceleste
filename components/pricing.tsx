83bf894cfebf7f3a67bf39b635a2571c22c58fd6
Maxime Celeste <maxime.celeste1221@gmail.com>
2025-06-11 11:44:12 +0400

    add logo

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -25,109 +25,111 @@ export default function Pricing() {
         </div>
 
         <div className="mt-8 grid gap-6 md:mt-20 md:grid-cols-3">
-          {/* Starter Pack */}
-          <Card className="flex flex-col justify-between p-6">
+          <Card className="flex flex-col">
             <CardHeader>
-              <CardTitle className="font-medium">Starter</CardTitle>
-              <span className="my-3 block text-2xl font-semibold">800 €</span>
-              <CardDescription className="text-sm">
-                Idéal pour les projets simples
-              </CardDescription>
+              <CardTitle className="font-medium">Free</CardTitle>
+              <span className="my-3 block text-2xl font-semibold">$0 / mo</span>
+              <CardDescription className="text-sm">Per editor</CardDescription>
             </CardHeader>
 
-            <CardContent className="flex-1 space-y-4">
-              <hr className="border-dashed w-full" />
-              <ul className="list-inside space-y-3 text-sm">
+            <CardContent className="space-y-4">
+              <hr className="border-dashed" />
+
+              <ul className="list-outside space-y-3 text-sm">
                 {[
-                  "Site vitrine (1–3 pages)",
-                  "Design responsive",
-                  "Formulaire de contact",
-                  "SEO de base",
-                  "Livraison en 7–10 jours",
+                  "Basic Analytics Dashboard",
+                  "5GB Cloud Storage",
+                  "Email and Chat Support",
                 ].map((item, index) => (
-                  <li key={index} className="flex items-start gap-2">
-                    <Check className="size-3 mt-1" />
+                  <li key={index} className="flex items-center gap-2">
+                    <Check className="size-3" />
                     {item}
                   </li>
                 ))}
               </ul>
             </CardContent>
 
-            <CardFooter>
+            <CardFooter className="mt-auto">
               <Button asChild variant="outline" className="w-full">
                 <Link href="">Get Started</Link>
               </Button>
             </CardFooter>
           </Card>
 
-          {/* Pro Pack */}
-          <Card className="relative flex flex-col justify-between p-6">
+          <Card className="relative">
             <span className="bg-linear-to-br/increasing absolute inset-x-0 -top-3 mx-auto flex h-6 w-fit items-center rounded-full from-purple-400 to-amber-300 px-3 py-1 text-xs font-medium text-amber-950 ring-1 ring-inset ring-white/20 ring-offset-1 ring-offset-gray-950/5">
-              Populaire
+              Popular
             </span>
 
-            <CardHeader>
-              <CardTitle className="font-medium">Pro</CardTitle>
-              <span className="my-3 block text-2xl font-semibold">1 300 €</span>
-              <CardDescription className="text-sm">
-                Parfait pour les TPE/PME
-              </CardDescription>
-            </CardHeader>
+            <div className="flex flex-col">
+              <CardHeader>
+                <CardTitle className="font-medium">Pro</CardTitle>
+                <span className="my-3 block text-2xl font-semibold">
+                  $19 / mo
+                </span>
+                <CardDescription className="text-sm">
+                  Per editor
+                </CardDescription>
+              </CardHeader>
 
-            <CardContent className="flex-1 space-y-4">
-              <hr className="border-dashed w-full" />
-              <ul className="list-inside space-y-3 text-sm">
-                {[
-                  "Site 5–7 pages ou petit e-commerce (≤ 20 produits)",
-                  "Design sur mesure",
-                  "CMS + Paiement en ligne",
-                  "Newsletter & SEO optimisé",
-                  "Support 15 jours",
-                ].map((item, index) => (
-                  <li key={index} className="flex items-start gap-2">
-                    <Check className="size-3 mt-1" />
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </CardContent>
+              <CardContent className="space-y-4">
+                <hr className="border-dashed" />
+                <ul className="list-outside space-y-3 text-sm">
+                  {[
+                    "Everything in Free Plan",
+                    "5GB Cloud Storage",
+                    "Email and Chat Support",
+                    "Access to Community Forum",
+                    "Single User Access",
+                    "Access to Basic Templates",
+                    "Mobile App Access",
+                    "1 Custom Report Per Month",
+                    "Monthly Product Updates",
+                    "Standard Security Features",
+                  ].map((item, index) => (
+                    <li key={index} className="flex items-center gap-2">
+                      <Check className="size-3" />
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
 
-            <CardFooter>
-              <Button asChild className="w-full">
-                <Link href="">Get Started</Link>
-              </Button>
-            </CardFooter>
+              <CardFooter>
+                <Button asChild className="w-full">
+                  <Link href="">Get Started</Link>
+                </Button>
+              </CardFooter>
+            </div>
           </Card>
 
-          {/* Startup Pack */}
-          <Card className="flex flex-col justify-between p-6">
+          <Card className="flex flex-col">
             <CardHeader>
-              <CardTitle className="font-medium">Entreprise</CardTitle>
-              <span className="my-3 block text-2xl font-semibold">2500€</span>
-              <CardDescription className="text-sm">
-                Solution complète sur mesure
-              </CardDescription>
+              <CardTitle className="font-medium">Startup</CardTitle>
+              <span className="my-3 block text-2xl font-semibold">
+                $29 / mo
+              </span>
+              <CardDescription className="text-sm">Per editor</CardDescription>
             </CardHeader>
 
-            <CardContent className="flex-1 space-y-4">
-              <hr className="border-dashed w-full" />
-              <ul className="list-inside space-y-3 text-sm">
+            <CardContent className="space-y-4">
+              <hr className="border-dashed" />
+
+              <ul className="list-outside space-y-3 text-sm">
                 {[
-                  "Site, app mobile ou SaaS sur mesure",
-                  "Admin personnalisé + authentification",
-                  "Connexion API / base de données",
-                  "Mise en ligne stores + formation",
-                  "Maintenance offerte 1 mois",
+                  "Everything in Pro Plan",
+                  "5GB Cloud Storage",
+                  "Email and Chat Support",
                 ].map((item, index) => (
-                  <li key={index} className="flex items-start gap-2">
-                    <Check className="size-3 mt-1" />
+                  <li key={index} className="flex items-center gap-2">
+                    <Check className="size-3" />
                     {item}
                   </li>
                 ))}
               </ul>
             </CardContent>
 
-            <CardFooter>
+            <CardFooter className="mt-auto">
               <Button asChild variant="outline" className="w-full">
                 <Link href="">Get Started</Link>
               </Button>
